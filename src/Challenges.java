7c22d4901624704ff02ae6b5d3115f65612120be
Ramiz Raja <informramiz@gmail.com>
2017-11-09 16:40:54 +0500

    chore: Create a Java/Kotlin project

---
+++ src/Challenges.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ramiz on 11/9/17.
+ */
+public class Challenges {
+    public static void main(String args[]) {
+
+    }
+}
